5e87aa1f01caecc6615fccbcc35bd1690794d56b
lipeng <lipeng@moyi365.com>
2019-03-05 14:39:45 +0800

    注册ui

--- app/src/main/java/com/muzi/repairtime/activity/RegisterActivity.java
+++ app/src/main/java/com/muzi/repairtime/activity/RegisterActivity.java
@@ -1,5 +1,7 @@
 package com.muzi.repairtime.activity;
 
+import android.view.View;
+
 import com.muzi.repairtime.R;
 import com.muzi.repairtime.activity.base.BaseActivity;
 import com.muzi.repairtime.databinding.ActivityRegisterBinding;
@@ -16,6 +18,29 @@ public class RegisterActivity extends BaseActivity<ActivityRegisterBinding> {
 
     @Override
     protected void init() {
+        getBinding().setClick(new ClickHandler());
+    }
+
+    public class ClickHandler {
+
+        /**
+         * 直接登录
+         *
+         * @param view
+         */
+        public void login(View view) {
+            finish();
+        }
+
+        /**
+         * 注册
+         *
+         * @param view
+         */
+        public void register(View view) {
+
+        }
 
     }
+
 }
