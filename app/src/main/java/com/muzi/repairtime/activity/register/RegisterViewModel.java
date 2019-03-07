23f1389bf2d9ec8d8facc6b33cb3667dcc4ae7f4
lipeng <lipeng@moyi365.com>
2019-03-07 10:22:23 +0800

    整理ui

--- app/src/main/java/com/muzi/repairtime/activity/register/RegisterViewModel.java
+++ app/src/main/java/com/muzi/repairtime/activity/register/RegisterViewModel.java
@@ -1,10 +1,14 @@
 package com.muzi.repairtime.activity.register;
 
 import android.app.Application;
+import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 
 import com.muzi.repairtime.activity.base.BaseViewModel;
+import com.muzi.repairtime.command.BindingCommand;
+import com.muzi.repairtime.command.BindingConsumerAction;
 
 /**
  * 作者: lipeng
@@ -14,9 +18,29 @@ import com.muzi.repairtime.activity.base.BaseViewModel;
  */
 public class RegisterViewModel extends BaseViewModel {
 
+    public ObservableField<String> userName = new ObservableField<>("");
+
+    public ObservableField<String> phone = new ObservableField<>("");
+
+    public ObservableField<String> password = new ObservableField<>("");
+
+    public ObservableField<String> confirmPsd = new ObservableField<>("");
+
+    public ObservableField<String> company = new ObservableField<>("");
+
     public RegisterViewModel(@NonNull Application application) {
         super(application);
     }
+
+    /**
+     * 输入手机号
+     */
+    public BindingCommand<String> phoneCommand = new BindingCommand<>(new BindingConsumerAction<String>() {
+        @Override
+        public void call(String s) {
+            Log.e("RegisterViewModel", s);
+        }
+    });
 
     /**
      * 直接登录
@@ -24,7 +48,7 @@ public class RegisterViewModel extends BaseViewModel {
      * @param view
      */
     public void login(View view) {
-
+        finish();
     }
 
     /**
